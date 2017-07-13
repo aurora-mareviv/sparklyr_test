056fc599503d58d607834845c6cdf08b59e2c27b
aurora-mareviv <mariauror@gmail.com>
2017-07-14 00:00:17 +0200

    Updating R application

--- sparklyr_script.R
+++ sparklyr_script.R
@@ -3,7 +3,6 @@ lapply(x, require, character.only = TRUE)
 
 # overwrites the previous sc to give access to sparklyr methods
 sc <- spark_connect(master = "yarn-client", spark_home = "/usr/hdp/2.4.2.0-258/spark")
-sqlContext = HiveContext(sc)
 
 # load database
 jscars <- spark_read_json(sc, name = "jscars", path = "amazon/jscars.json")
